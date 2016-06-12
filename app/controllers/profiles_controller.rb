56504c036b2ee12434986be43e2dcffcb48705b5
Jessica Jones <jjones@demacmedia.com>
2016-06-12 11:30:15 -0400

    Add profiles controller and views

---
+++ app/controllers/profiles_controller.rb
@@ -0,0 +1,10 @@
+class ProfilesController < ApplicationController
+  def new
+  end
+
+  def edit
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
 
+  get 'profiles/new'
+
+  get 'profiles/edit'
+
+  get 'profiles/show'
+
   devise_for :users
 	
 	root to: "home#index"
