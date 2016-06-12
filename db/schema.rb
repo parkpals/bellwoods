7ec8934c94231fce014c0c5d668dc234e0fdb3a4
Jessica Jones <jjones@demacmedia.com>
2016-06-12 12:04:54 -0400

    Add user id column to profile

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160612152448) do
+ActiveRecord::Schema.define(version: 20160612155617) do
 
   create_table "profiles", force: :cascade do |t|
     t.string   "name"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20160612152448) do
     t.string   "avatar_content_type"
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
