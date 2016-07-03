28c76696d6959f0ad02e664f06a1d2f824b0f895
Robin Hamill <hamillrobin@gmail.com>
2016-07-03 12:12:43 -0400

    Correct error with invite mailer (finally)

--- app/mailers/invite_mailer.rb
+++ app/mailers/invite_mailer.rb
@@ -2,12 +2,13 @@ class InviteMailer < ApplicationMailer
 	require 'mailgun'
 
 	# Change this
-	default from: "hamillrobin@gmail.com"
+	default from: "bookbuddies@sandboxdb186207bc4648cb947c92d270219a9e.mailgun.org"
+	# You will have to change this!!
 
 	def meet_invite(invite)
 		@invite = invite
-		mail  to: @invite.recipient,
-					subject: "Parkpals invite"
+		mail to: @invite.recipient,
+		subject: "Parkpals invite"
 	end
 
 end
