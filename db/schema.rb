c7e7b5517271540a42e4723e0ea3e1023626a19e
Jessica Jones <jjones@demacmedia.com>
2016-07-03 11:32:58 -0400

    Add provider and uid to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160612155617) do
+ActiveRecord::Schema.define(version: 20160703153242) do
 
   create_table "invites", force: :cascade do |t|
     t.integer  "latitude"
@@ -47,6 +47,8 @@ ActiveRecord::Schema.define(version: 20160612155617) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "provider"
+    t.string   "uid"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
