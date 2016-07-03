764a4fea817d37d2ad2ca18cc82d99f8d19adc3c
Jessica Jones <jjones@demacmedia.com>
2016-07-03 11:54:28 -0400

    Add twitter omniauth to routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resource :profile
   resources :invites
 
-  devise_for :users, :controllers => { registrations: 'users/registrations' }
+  devise_for :users, :controllers => { registrations: 'users/registrations', omniauth_callbacks: "users/omniauth_callbacks" }
 	
   root to: "home#index"
 
