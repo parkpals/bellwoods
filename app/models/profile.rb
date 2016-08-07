438a9bb2288a49d36bd8e14b2fcf58552f92d15e
Robin Hamill <hamillrobin@gmail.com>
2016-08-07 16:32:50 -0600

    Add method which concats :first_name + :last_name

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -5,4 +5,8 @@ class Profile < ActiveRecord::Base
 	
 	has_attached_file :avatar, :default_url => 'avatar.jpg', styles: { medium: "250x250#", thumb: "150x150#" }
 	validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+
+	def full_name
+		self.first_name.to_s + self.last_name.to_s
+	end
 end
