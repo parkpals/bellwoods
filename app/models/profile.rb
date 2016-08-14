1eebc7fb056baeaeb37636f871a8c1de48469626
Jessica Jones <jjones@demacmedia.com>
2016-08-14 13:48:42 -0400

    Add user avatar as map marker in show view

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -4,7 +4,7 @@ class Profile < ActiveRecord::Base
 
 	belongs_to :user
 	
-	has_attached_file :avatar, :default_url => 'avatar.jpg', styles: { medium: "250x250#", thumb: "150x150#" }
+	has_attached_file :avatar, :default_url => 'avatar.jpg', styles: { medium: "250x250#", thumb: "150x150#", marker: "55x55#" }
 	validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 
 	def full_name
