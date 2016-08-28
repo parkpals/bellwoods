85c304dd0bf2ebb63036ef09737805c042534c4d
Robin Hamill <hamillrobin@gmail.com>
2016-08-28 12:10:03 -0400

    Validate email in form, and on model

--- app/models/invite.rb
+++ app/models/invite.rb
@@ -1,6 +1,7 @@
 class Invite < ActiveRecord::Base
 
 	validates :recipient, presence: true
+	validates_format_of :recipient,:with => Devise::email_regexp
 	validates :message, presence: true
 	validates :latitude, presence: true
 	validates :longitude, presence: true
@@ -11,5 +12,9 @@ class Invite < ActiveRecord::Base
 		expire_time = self.expire_time
 		((expire_time.hours.since self.created_at) - Time.now)
 	end
+
+  def email_to_arr
+    self.recipient.split(/[,\s]+/)
+  end
 
 end
