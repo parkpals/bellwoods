ca1dfad401e4d60c72fdb09dc6e0211a01fe1695
Robin Hamill <hamillrobin@gmail.com>
2017-07-28 16:53:18 -0400

    ESLint changes

--- app/assets/javascripts/nav.js
+++ app/assets/javascripts/nav.js
@@ -3,12 +3,12 @@ $(function() {
   const $header = $('header');
   const $navBtn = $('#nav');
   $navBtn.on('click', function() {
-		$header.toggleClass('nav_open');
-	});
+    $header.toggleClass('nav_open');
+  });
 
-	$body.keyup(function(e) {
-		if (e.keyCode === 27) {
-			$header.removeClass('nav_open');
-		}
-	});
+  $body.keyup(function(e) {
+    if (e.keyCode === 27) {
+      $header.removeClass('nav_open');
+    }
+  });
 });
